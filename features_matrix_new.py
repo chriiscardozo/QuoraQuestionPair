c1c63c242b1a5e23d7c36948792564d15843e7de
Christian Cardozo <chriiscardozo@gmail.com>
2017-05-31 18:33:13 -0300

    generating generate_test_features to new matrix feat

--- features_matrix_new.py
+++ features_matrix_new.py
@@ -108,3 +108,76 @@ def generate_train_features(n_examples):
 
 def generate_test_features():
 	print("*** generating matrix test features ***")
+
+	corpus1 = []
+	corpus2 = []
+	corpus3 = []
+	all_words = {}
+	st = PorterStemmer()
+	append_to_X1 = []
+	append_to_X2 = []
+	append_to_X3 = []
+
+	i = 0
+
+	with open(Constants.TEST_TOKENIZED_FILE) as f:
+		csv_reader = csv.reader(f, delimiter=',', quotechar='"')
+		for index, line in enumerate(csv_reader):
+			q1 = line[1].split(' ')
+			q2 = line[2].split(' ')
+
+			features_equals_count = get_feature_count_equals_words(q1, q2)
+			append_to_X1.append([abs(len(q1)-len(q2))/len(q1), features_equals_count[0]])
+			append_to_X2.append([abs(len(q1)-len(q2))/len(q2), features_equals_count[1]])
+			append_to_X3.append([get_feature_jaccard_distance(q1, q2), get_feature_leveinshtein_distance(q1, q2)])
+
+			for q in [q1, q2]:
+				for token in q:
+					if token not in all_words:
+						all_words[token] = True
+			
+			equals_words = []
+			for t in q1:
+				if t in q2:
+					equals_words.append(t)
+
+			corpus1.append(' '.join(q1))
+			corpus2.append(' '.join(q2))
+			corpus3.append(' '.join(equals_words))
+
+			if(i % 1000 == 0): print(str(i) + '\r', end='')
+			i += 1
+
+		print('Total distinct words: ', len(all_words))
+		X_matrices = get_sparses_matrices(corpus1,corpus2,corpus3)
+
+		# uncoment for semantic features before SVD
+		# X_matrices[0] = hstack((X_matrices[0],append_to_X1))
+		# X_matrices[1] = hstack((X_matrices[1],append_to_X2))
+		# X_matrices[2] = hstack((X_matrices[2],append_to_X3))
+
+		for index,item in enumerate(X_matrices):
+			print('Dimensions in X' + str(index+1) + ' before SVD: ' + str(item.shape))
+
+			X_aux = item.tocsc()
+
+			X_truncated, Sigma, VT = sparsesvd(X_aux, 50)
+			X_matrices[index] = X_truncated.transpose()
+			print('New dimensions for X' + str(index+1) + ': ' + str(X_matrices[index].shape))
+			# plt.plot(Sigma)
+			# plt.show()
+
+		# uncoment for semantic features after SVD
+		X_matrices.append(append_to_X1)
+		X_matrices.append(append_to_X2)
+		X_matrices.append(append_to_X3)
+		X = np.concatenate(X_matrices,axis=1)
+
+		print('Final X dimentions:', X.shape)
+
+		with open(Constants.TEST_MATRIX_FEATURES, 'w') as f:
+			csv_writer = csv.writer(f)
+			for index, item in enumerate(X):
+				line = [index]
+				line.extend(item)
+				csv_writer.writerow(line)
